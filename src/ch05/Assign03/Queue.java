b20a14131e3b688ab2636d62b806d839a7549336
Florian Eiden <florian.eiden@fleid.fr>
2015-08-13 11:23:41 +0200

    Assign03

--- src/ch05/Assign03/Queue.java
+++ src/ch05/Assign03/Queue.java
@@ -10,9 +10,38 @@ package ch05.Assign03;
 
 public class Queue
 {
-
+    // Ici on doit initialiser l'objet pour pouvoir l'utiliser
+    // A mon avis parce que LinkedList n'a pas de constructeur, mais pas sur
     private LinkedList LL = new LinkedList();
 
 
+    // enqueue (push at the tail)
+    // dequeue (pop from the head)
+    // peek (from the head)
+
+    public void enqueue( int data )
+    {
+        this.LL.insertAtTail(data);
+    }
+
+    public int dequeue()
+    {
+        if (LL.length() > 0)
+        {
+            int currentData = this.LL.readFromHead();
+            this.LL.deleteFromHead();
+            return currentData;
+        }
+        return 0;
+    }
+
+    public int peek()
+    {
+        if (LL.length() > 0)
+        {
+            return this.LL.readFromHead();
+        }
+        return 0;
+    }
 
 }
